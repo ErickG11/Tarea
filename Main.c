62e3aefbec4d2856cb3f6aef28960bdd1f2acd10
ErickG11 <grandaerick8@gmail.com>
2023-01-20 17:17:02 -0500

    Deberr

--- Main.c
+++ Main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/*Reemplace en otro archivo los multiplos del 3*/
+
 int main(){
-    int num;
+    
     FILE *archivo=NULL;
     FILE *archivo2=NULL;
+    int num;
+
     archivo=fopen("vector.txt","r");
     archivo2=fopen("vector2.txt","w");
 
@@ -11,7 +13,9 @@ int main(){
         printf("No se puede abrir el archivo");
         return -1;
     }
-
+    for(int i=0; i<100; i++){
+        fprintf(archivo,"%d\n",i);
+    }
     while(!feof(archivo)){
         fscanf(archivo, "%d", &num);
     if(num%3==0){
@@ -21,11 +25,8 @@ int main(){
         fprintf(archivo2,"%d\n",num);
     }
     }
-
-
-    for(int i=0; i<100; i++){
-        fprintf(archivo,"%d\n",i);
-    }
     fclose(archivo);
+    fclose(archivo2);
+
      return 0;
 }
